710b23279442bea1ff69c26952a8ba7245510149
RK Reaz <skresat108@gmail.com>
2017-07-20 13:49:31 +0600

    Edit Activity Add

---
+++ app/src/main/java/com/educareapps/jsonreader/activity/EditActivity.java
@@ -0,0 +1,19 @@
+package com.educareapps.jsonreader.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.educareapps.jsonreader.R;
+
+/**
+ * Created by RK-REAZ on 7/20/2017.
+ */
+
+public class EditActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
